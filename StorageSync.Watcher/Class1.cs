6599f64a23e34a66ae8fa955e14e3af8ac4f8964
samlives <samlive@outlook.com>
2014-11-22 13:21:56 +0530

    Solution Added

---
+++ StorageSync.Watcher/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageSync.Watcher
+{
+    public class Class1
+    {
+    }
+}
