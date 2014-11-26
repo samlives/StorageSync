8921880e0a181a8ab7c22803eb189c1ba35ec82f
samlives <samlive@outlook.com>
2014-11-26 17:10:50 +0530

    Added Data Queue and watcher code

---
+++ Storage.Data/IQueueManager.cs
@@ -0,0 +1,33 @@
+﻿
+using StorageSync.IO.File;
+using System.Collections.Generic;
+namespace StorageSync.Watcher
+{
+    public class QueueManager : IQueueManager
+    {
+        private IQueueProvider _queueProvider;
+        public QueueManager(IQueueProvider queueProvider)
+        {
+            _queueProvider = queueProvider;
+        }
+        public ChangeFileContext AddToQueue(ChangeFileContext fileContext)
+        {            
+            return null;    
+        }
+        public ChangeFileContext UpdateQueue(ChangeFileContext fileContext)
+        {
+            return null;
+        }
+        public IList<ChangeFileContext> ReadQueue()
+        {
+            return null;
+        }
+    }
+
+    public interface IQueueManager
+    {
+        ChangeFileContext AddToQueue(ChangeFileContext fileContext);
+        ChangeFileContext UpdateQueue(ChangeFileContext fileContext);
+        IList<ChangeFileContext> ReadQueue();
+    }
+}
---
+++ Storage.Data/IQueueProvider.cs
@@ -0,0 +1,14 @@
+﻿using StorageSync.IO.File;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StorageSync.Watcher
+{
+    public interface IQueueProvider
+    {
+        bool AddToQueue(ChangeFileContext fileContext);
+        bool UpdateToQueue(ChangeFileContext fileContext);
+    }
+}
